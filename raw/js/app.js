40659a8d8a087e9586d718a2cb951f34afa91be4
martytheparty <marty@ilikeemail.com>
2017-12-26 13:53:36 -0500

    Start of adding characters.

--- raw/js/app.js
+++ raw/js/app.js
@@ -13,6 +13,7 @@ firebase.initializeApp(config);
 
 var specialRef = firebase.database().ref('special');
 var playersRef = firebase.database().ref('players');
+var playerRef;
 var playerlist = [];
 
 playersRef.on('value', function(snapshot) {
@@ -20,8 +21,9 @@ playersRef.on('value', function(snapshot) {
   playerlist = [];
   snapshot.forEach(function(childSnapshot){
     var player = childSnapshot.val();
+    var key = childSnapshot.key;
     html = html + player.playerName + '-' + player.p + '<br> \n';
-    playerlist.push({username:player.playerName,p:player.p});
+    playerlist.push({key: key, username:player.playerName,p:player.p});
   });
   $('#adminPlayerList').html(html);
 });
@@ -33,6 +35,7 @@ $('#loading').hide();
 $('#loaded').show();
 $('#secret').val('');
 $('#create-new-player').hide();
+$('#player-logged-in').hide();
 
 function enterDungeonMaster() {
   $('#new-player').hide();
@@ -59,16 +62,40 @@ function adminLogin() {
   );
 }
 function loginPlayer() {
+  var found = false;
   var username = $("#username").val();
   var p = $("#word").val();
   for(var i=0;i<playerlist.length;i++){
-    console.log(playerlist[i]);
+    if (playerlist[i].username === username && playerlist[i].p === p) {
+      $('#player-login').hide();
+      $('#player-logged-in').show();
 
+      $('#username').val("");
+      $('#word').val("");
+      found = true;
+      playerRef = firebase.database().ref('players/' + playerlist[i].key + '/characters');
+      playerRef.on('value', function(snapshot) {
+        console.log(snapshot);
+        alert('update the character list');
+      });
+      /*
+      to create a new character
+      playerRef.push({test:'test'})
+      */
+    }
   };
-  $('#username').val("");
-  $('#word').val("");
+
+  if (!found) {
+    alert(username + ' was not found');
+  }
 
 }
+
+function addCharacter() {
+  console.log(playerRef);
+  alert('add a character');
+}
+
 function showPlayerSelection() {
   $('#new-player').hide();
   $('#player-enter').hide();
